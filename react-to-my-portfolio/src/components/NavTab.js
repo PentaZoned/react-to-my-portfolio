bdb1c48146d76bc979f1d30109d270ab3626f3e4
Bradley Le <brabrele@gmail.com>
2022-03-02 20:29:58 -0800

    cleared default js content from all js files in src folder, imported react and the other files to the respective files

---
+++ react-to-my-portfolio/src/components/NavTab.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NavTabs() {
+
+}
+
+export default NavTabs;
---
+++ react-to-my-portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTab';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Footer from './Footer';
+
+export default function PortfolioContainer() {
+    
+}
