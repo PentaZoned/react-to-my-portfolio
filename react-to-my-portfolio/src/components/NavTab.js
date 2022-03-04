53541328db33f23e938e53f614fb867f2a3677a8
Bradley Le <brabrele@gmail.com>
2022-03-04 04:27:30 -0800

    installed bootstrap package to use classes in the components, created testing JSX for navtabs

--- react-to-my-portfolio/src/components/NavTab.js
+++ react-to-my-portfolio/src/components/NavTab.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-function NavTabs() {
-
+function NavTabs({currentPage, handlePageChange}) {
+    return (
+        <ul className='nav nav-tabs'>
+            <li className='nav-item'>
+            <a href='#about' onClick={() => handlePageChange('About')}
+                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
+                About
+            </a>
+            </li>
+        </ul>
+    );
 }
 
 export default NavTabs;
