d49a4c05f887f673ada8e516e5881dc9747d006b
Bradley Le <brabrele@gmail.com>
2022-03-04 07:32:23 -0800

    added Contact section with form

--- react-to-my-portfolio/src/components/NavTab.js
+++ react-to-my-portfolio/src/components/NavTab.js
@@ -4,10 +4,17 @@ function NavTabs({currentPage, handlePageChange}) {
     return (
         <ul className='nav nav-tabs'>
             <li className='nav-item'>
-            <a href='#about' onClick={() => handlePageChange('About')}
-                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
-                About
-            </a>
+                <a href='#about' onClick={() => handlePageChange('About')}
+                    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
+                    About Me
+                </a>
+            </li>
+
+            <li className='nav-item'>
+                <a href='#contact' onClick={() => handlePageChange('Contact')}
+                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                    Contact
+                </a>
             </li>
         </ul>
     );
--- react-to-my-portfolio/src/components/pages/Contact.js
+++ react-to-my-portfolio/src/components/pages/Contact.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import '../styles/Contact.css';
 
 function Contact() {
-
+    return (
+        <section id="contact" class="p-2 text-center">
+            <h2>Contact Me</h2>
+            <form>
+                <label for="name">Name: </label><br />
+                <input type="text" id="name" name="name" placeholder='John Doe'/><br /><br />
+                <label for="lname">Email:</label><br />
+                <input type="email" id="email" name="email" placeholder='johndoe@gmail.com'/><br /><br />
+                <label for="lname">Message:</label><br />
+                <input type="text" id="message" name="message" placeholder='Hello John Doe.'/><br /><br />
+                <button>Submit</button>
+            </form>
+        </section>
+    );
 }
 
 export default Contact;
