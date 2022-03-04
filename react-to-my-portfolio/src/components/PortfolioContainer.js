747c3e676f9b85c9bc45484005d861fa400b33b7
Bradley Le <brabrele@gmail.com>
2022-03-04 03:52:20 -0800

    enabled PortfolioContainer() to return the NavTabs, the conditional function renderPage() and the Footer

--- react-to-my-portfolio/src/components/PortfolioContainer.js
+++ react-to-my-portfolio/src/components/PortfolioContainer.js
@@ -7,5 +7,30 @@ import Resume from './pages/Resume';
 import Footer from './Footer';
 
 export default function PortfolioContainer() {
-    
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        if(currentPage === 'About') {
+            return <About />
+        }
+        if(currentPage === 'Contact') {
+            return <Contact />
+        }
+        if(currentPage === 'Portfolio') {
+            return <Portfolio />
+        }
+        if(currentPage === 'Resume') {
+            return <Resume />
+        }
+    }
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            {<NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />}
+            {renderPage()}
+            {<Footer />}
+        </div>
+    );
 }
