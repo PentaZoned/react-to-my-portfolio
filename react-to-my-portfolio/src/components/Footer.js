08336ef7d5bd5c0b7e9c186daebab6fc5a46a78c
Bradley Le <brabrele@gmail.com>
2022-03-04 04:32:38 -0800

    added footer, changed basic information on the index.html such as title

--- react-to-my-portfolio/src/components/Footer.js
+++ react-to-my-portfolio/src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Footer() {
-
+    return (
+        <footer className='text-center p-3'>
+            &copy 2022 Bradley Le
+        </footer>
+    );
 }
 
 export default Footer;
