1a7261773999ce972aa1f01fdc95a8de72bc9df7
Bradley Le <brabrele@gmail.com>
2022-03-04 19:17:27 -0800

    added 3 new projects to the projects section

--- react-to-my-portfolio/src/components/pages/Portfolio.js
+++ react-to-my-portfolio/src/components/pages/Portfolio.js
@@ -2,24 +2,43 @@ import React from 'react';
 import FeedMe from '../images/feed-me.jpg';
 import WeatherDashboard from '../images/weatherDashboard.jpg';
 import NoteTaker from '../images/noteTaker.jpg';
+import TextEditor from '../images/textEditor.jpg';
+import Dontfundme from '../images/dontfundme.jpg';
+import WorkdayScheduler from '../images/workdayScheduler.jpg';
 import '../styles/Project.css';
 
 const currentProjects = [
     {
         title: 'Restaurant Locater - "Feed Me"',
         deployed: "https://pentazoned.github.io/feed-me/",
-        repo: "https://github.com/PentaZoned/feed-me"
+        repo: "https://github.com/PentaZoned/feed-me",
     },
     {
         title: 'Weather Dashboard',
         deployed: "https://pentazoned.github.io/weather-dashboard/",
-        repo: "https://github.com/PentaZoned/weather-dashboard"
+        repo: "https://github.com/PentaZoned/weather-dashboard",
     },
     {
         title: 'Note Taker',
         deployed: "https://shielded-shelf-75023.herokuapp.com/",
-        repo: "https://github.com/PentaZoned/note-taker-sacred-treasure"
-    }
+        repo: "https://github.com/PentaZoned/note-taker-sacred-treasure",
+    },
+    {
+        title: 'DontFundMe',
+        deployed: 'https://still-headland-20752.herokuapp.com/',
+        repo: 'https://github.com/PentaZoned/DontFundMe',
+    },
+    {
+        title: 'My Own Text Editor',
+        deployed: 'https://whispering-woodland-68116.herokuapp.com/',
+        repo: 'https://github.com/PentaZoned/my-own-text-editor',
+    },
+    {
+        title: 'Work Dat Scheduler',
+        deployed: 'https://pentazoned.github.io/work-day-scheduler/',
+        repo: 'https://github.com/PentaZoned/work-day-scheduler',
+    },
+
 ]
 
 const styles = {
@@ -71,12 +90,60 @@ function Portfolio() {
                 </h4>
 
                 <p class="text-center">
-                    <a href={currentProjects[1].deployed} style={styles.links}>Deployed Link</a> | 
+                    <a href={currentProjects[2].deployed} style={styles.links}>Deployed Link</a> | 
                     <a href={currentProjects[2].repo} style={styles.links}>Github Repository</a>
                 </p>
 
                 <div class="d-flex justify-content-center pb-5">
                     <img src={NoteTaker} class="img-fluid order-1" alt="screenshot of Note Taker app" />
+                </div>
+            </div>
+            <hr />
+            <div>
+                <h4 class="text-center pb-2 p2-5">
+                    {currentProjects[3].title}
+                </h4>
+
+                <p class="text-center">
+                    <a href={currentProjects[3].deployed} style={styles.links}>Deployed Link</a> | 
+                    <a href={currentProjects[3].repo} style={styles.links}>Github Repository</a>
+                </p>
+
+                <div class="d-flex justify-content-center pb-5">
+                    <img src={Dontfundme} class="img-fluid order-1"
+                        alt="screenshot of fundraising app" />
+                </div>
+            </div>
+            <hr />
+            <div>
+                <h4 class="text-center pb-2 p2-5">
+                    {currentProjects[4].title}
+                </h4>
+
+                <p class="text-center">
+                    <a href={currentProjects[4].deployed} style={styles.links}>Deployed Link</a> | 
+                    <a href={currentProjects[4].repo} style={styles.links}>Github Repository</a>
+                </p>
+
+                <div class="d-flex justify-content-center pb-5">
+                    <img src={TextEditor} class="img-fluid order-1"
+                        alt="screenshot of the Text Editor" />
+                </div>
+            </div>
+            <hr />
+            <div>
+                <h4 class="text-center pb-2 p2-5">
+                    {currentProjects[5].title}
+                </h4>
+
+                <p class="text-center">
+                    <a href={currentProjects[5].deployed} style={styles.links}>Deployed Link</a> | 
+                    <a href={currentProjects[5].repo} style={styles.links}>Github Repository</a>
+                </p>
+
+                <div class="d-flex justify-content-center pb-5">
+                    <img src={WorkdayScheduler} class="img-fluid order-1"
+                        alt="screenshot of the calendar for the workday app" />
                 </div>
             </div>
         </section>
